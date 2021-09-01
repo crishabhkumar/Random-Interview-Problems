757dfc1fa4934937d441ea7b3eabd478063324c1
Rishabh Kumar <70275515+crishabhkumar@users.noreply.github.com>
2021-09-01 22:34:03 +0530

    Update NumberPattern.cpp

--- NumberPattern.cpp
+++ NumberPattern.cpp
@@ -1,7 +1,16 @@
+/*Print the Number pattern
+
+   1
+  23
+ 345
+4567
+*/
+
 #include<iostream>
 using namespace std;
 int main(){
-	cout<<"NumberPattern:\n";
+	//First Approach
+	cout<<"---First Approach---\n";
 	int totalrows = 4;
 	int row = 1;
 	while(row <= totalrows){
@@ -17,6 +26,25 @@ int main(){
 			col++;
 		}
 		row++;
+		cout<<"\n";
+	}
+	
+	//Second Approach
+	cout<<"\n----Second Approach----\n";
+	int totalrows_2 = 4;
+	int row_2 = 1;
+	while(row_2 <= totalrows_2){
+		int spaces = 1;
+		while(spaces < totalrows_2 - row_2){
+			cout<<" ";
+			spaces++;
+		}
+		int col_2 = 1;
+		while(col_2 <= row_2){
+			cout<<(row_2+col_2-1);
+			col_2++;
+		}
+		row_2++;
 		cout<<"\n";
 	}
 }
